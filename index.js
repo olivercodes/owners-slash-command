e3cd760d40d91622419843e08cf11d456f3e21f0
olivercodes <boliver@linux.com>
2021-06-09 02:01:06 -0400

    dist

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const {getUserIsApprover} = require('./lib/index');
+const {getUserIsApprover} = require('./lib/helpers');
 
 try {
   const command = core.getInput('command');
   const actor = github.context.actor;
-  return getUserIsApprover(actor, './');
+  return getUserIsApprover(actor, '.');
 } catch (error) {
   core.setFailed(error.message);
 }
