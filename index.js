f0a551c8041eaf146580e9c5328d8b5c85626d1c
Bryan Oliver <boliver@linux.com>
2022-08-11 10:56:01 -0400

    feat: refactored for teams api

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const {getUserIsApprover} = require('./lib/helpers');
 
-try {
-  const actor = github.context.actor;
-  const isApprover = getUserIsApprover(actor, '.');
-  if (!isApprover) {
-    core.setFailed(`${actor} is not an approver. Please check your OWNERS file.`)
-  } else {
-    return true;
+(async () => {
+
+  // const actor = 'username';
+  // const repo = 'repo';
+  // const org = 'org';
+  const { actor, repo, org } = github.context;
+  try {
+    const isApprover = await getUserIsApprover(org, repo, actor);
+    if (!isApprover) {
+      core.setFailed(`${actor} is not an approver`);
+    } else return true
+  } catch (error) {
+    core.setFailed(error.message);
   }
-} catch (error) {
-  core.setFailed(error.message);
-}
+  console.log(isApprover);
+
+
+})();
+
